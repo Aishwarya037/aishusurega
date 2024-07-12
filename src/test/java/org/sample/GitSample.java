2b454b89f27636218210ef1b0d23931dd68cdf0a
Aishwarya037 <aishubaskaran03@gmail.com>
2024-07-12 10:48:10 +0530

    sample code

---
+++ src/test/java/org/sample/GitSample.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class GitSample {
+	 public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+		System.out.println("test5");
+	}
+
+}
