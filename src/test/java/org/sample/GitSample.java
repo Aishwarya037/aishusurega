8585052dfe9b83eab579702fa3a1eba1d9b9a62b
Aishvarya123456 <uaishvarya@gmail.com>
2024-07-12 11:12:12 +0530

    surega code

--- src/test/java/org/sample/GitSample.java
+++ src/test/java/org/sample/GitSample.java
@@ -3,8 +3,8 @@ package org.sample;
 public class GitSample {
 	 public static void main(String[] args) {
 		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
+		System.out.println("test 12");
+		System.out.println("test 13");
 		System.out.println("test4");
 		System.out.println("test5");
 	}
